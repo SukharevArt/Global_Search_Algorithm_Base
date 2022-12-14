ef7d0165ba1141db23931f685c375b7af3d8e8b0
Sukharev <artem200340447@gmail.com>
2022-12-15 00:11:52 +0300

    Создание рабочего информационно - статистический алгоритма с локальной настройкой

--- Global_Search_Algorithm_Base/Global_Search_Algorithm_Base.cpp
+++ Global_Search_Algorithm_Base/Global_Search_Algorithm_Base.cpp
@@ -21,7 +21,7 @@ struct tpoint {
 	}
 };
 
-class GSA
+class IGOAWLT
 {
 protected:
 	vector < tpoint > memory_points;
@@ -48,17 +48,18 @@ public:
 		base_calculates();
 		for ( ;number_of_tests < count_of_tests; ){
 			numeration_and_sort();
-			long double value = calculate_expected_const();
-			calculate_haracteristecs(value);
+			long double valueM = calculate_expected_const();
+			long double valueX = calculate_max_len();
+			calculate_haracteristecs(valueM,valueX);
 			list<tpoint>::iterator interval = choice_best_interval();
 			list<tpoint>::iterator prev = std::prev(interval);
 
 			if ((*interval).X - (*prev).X < errorX*(borders.second-borders.first))
 				break;
-			long double new_pos = calculate_position_of_point(interval, value);
+			long double new_pos = calculate_position_of_point(interval,valueM,valueX);
 			test_point(new_pos,interval);
 		}
-		cout << "number of operations : " << number_of_tests  << ", result = " << result << "\n";
+		cout << "number of operations : " << number_of_tests+1  << ", result = " << result << "\n";
 		/*numeration_and_sort();
 		for (auto x : points)
 			cout << "X=" << x.X << ", Y=" << x.Y << "\n";*/
@@ -66,7 +67,7 @@ public:
 		return result;
 	}
 
-	GSA(long double _parametr = 2, long double _errorX = 0.001, long double leftborder = 0, long double rightborder = 1)
+	IGOAWLT(long double _parametr = 2, long double _errorX = 0.001, long double leftborder = 0, long double rightborder = 1)
 		:parametr(_parametr), errorX(_errorX), borders({ leftborder,rightborder }) {
 		result = UINT64_MAX;
 		size = 0;
@@ -74,6 +75,7 @@ public:
 	}
 
 private:
+	//+
 	void test_point(long double X,list<tpoint>::iterator nxt) {
 		long double Y = function(X);
 		size++;
@@ -84,7 +86,7 @@ private:
 		memory_points.push_back(tpoint(X, Y));
 		
 	}
-
+	//+
 	void base_calculates() {
 		size = 0;
 		points.clear();
@@ -96,24 +98,30 @@ private:
 	void numeration_and_sort() {
 		//sort(points.begin(), points.end());
 	}
-	
+	//+
 	long double calculate_expected_const() {
-		long double value = 0;
+		long double valueM = 0;
 		list<tpoint>::iterator it = points.begin();
 		for ( int i = 0; i < size - 1; i++, it++) {
 			auto tmp = *it;
 			auto nxt = *next(it);
-			value = max(value,
-			abs((nxt.Y - tmp.Y) / (nxt.X - tmp.X)));
+			valueM = max(valueM,abs((nxt.Y - tmp.Y) / (nxt.X - tmp.X)));
 		}
-		if (value == 0) 
-			value = 1;
-		else
-			value *= parametr;
-		return value;
+		return valueM;
 	}
-	
-	void calculate_haracteristecs(long double value) {
+	//+
+	long double calculate_max_len() {
+		long double valueX = 0;
+		list<tpoint>::iterator it = points.begin();
+		for ( int i = 0; i < size - 1; i++, it++) {
+			auto tmp = *it;
+			auto nxt = *next(it);
+			valueX = max(valueX,abs(nxt.X - tmp.X));
+		}
+		return valueX;
+	}
+	//+
+	void calculate_haracteristecs(long double valueM,long double valueX) {
 		characteristics.resize(size);
 		characteristics[0] = -1;
 		list<tpoint>::iterator it = points.begin();
@@ -123,11 +131,30 @@ private:
 			long double diffX = nxt.X-tmp.X;
 			long double diffY = nxt.Y-tmp.Y;
 			long double sumY = nxt.Y+tmp.Y;
-			long double R = value * diffX+diffY*diffY/(value*diffX)-2*sumY;
+			long double value_m = calculate_m(it, valueM, valueX);
+
+			long double R = value_m*parametr * diffX+diffY*diffY/(parametr*value_m*diffX)-2*sumY;
 			characteristics[i] = R;
 		}
 	}
-	
+	//+
+	long double calculate_m(list<tpoint>::iterator tmp, long double valueM, long double valueX) {
+		auto it2 = tmp;
+		auto it3 = next(tmp);
+		auto it4 = next(it3);
+
+		long double value_m = valueM * ((*it3).X-(*it2).X) / valueX;
+		if (it2 != points.begin()) {
+			auto it1 = prev(tmp);
+			value_m = max(value_m,abs((*it2).Y-(*it1).Y)/((*it2).X - (*it1).X));
+		}
+		value_m = max(value_m,abs((*it3).Y-(*it2).Y)/((*it3).X - (*it2).X));
+		if (it4 != points.end()) {
+			value_m = max(value_m,abs((*it4).Y-(*it3).Y)/((*it4).X - (*it3).X));
+		}
+		return value_m;
+	}
+	//+
 	list<tpoint>::iterator choice_best_interval() {
 		long double Max = characteristics[1];
 		auto it = next(points.begin());
@@ -141,11 +168,15 @@ private:
 		}
 		return interval;
 	}
-
-	long double calculate_position_of_point(list<tpoint>::iterator interval,long double value) {
+	//+
+	long double calculate_position_of_point(list<tpoint>::iterator interval, long double valueM, long double valueX) {
 		list<tpoint>::iterator prev = std::prev(interval);
+		long double optvalue_m = calculate_m(prev, valueM, valueX);
+		if (optvalue_m == 0) {
+			return ((*interval).X + (*prev).X) / 2;
+		}
 		return ((*interval).X + (*prev).X) / 2 -
-			((*interval).Y - (*prev).Y) / (2 * value);
+			((*interval).Y - (*prev).Y) / (2 *parametr* optvalue_m);
 	}
 	
 };
@@ -153,7 +184,7 @@ private:
 int main()
 {	
 	//GSA(long double _parametr = 2, long double _errorX = 0.001, long double leftborder = 0, long double rightborder = 1)
-	GSA tmp(2, 0.001, -1.2, 2.0);
+	IGOAWLT tmp(2, 0.001, -1.2, 2.0);
 	cout << tmp.calculate_minimum()<<"\n";
 
 }
